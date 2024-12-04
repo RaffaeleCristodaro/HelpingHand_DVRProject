e0d1d251353d8c295339e8d36523fccd401dd14e
FilippoCaio <filippocaio.workspace@gmail.com>
2024-12-04 15:19:58 +0100

    Modified PG3 anim blueprint

---
+++ Source/HelpingHand_NewDV/HelpingHand_NewDV.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HelpingHand_NewDV.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HelpingHand_NewDV, "HelpingHand_NewDV" );
---
+++ Source/HelpingHand_NewDV/Private/Header.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Header.h"
+
+// Sets default values
+AHeader::AHeader()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHeader::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHeader::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
